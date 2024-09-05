2b7ca9ce965cb86ab95d56d29ff4982d4d86aa9d
gk <demar.demidov@yandex.ru>
2024-09-05 19:31:28 +0300

    test

---
+++ GarbageCollector/src/main/java/AutoCollection.java
@@ -0,0 +1,69 @@
+public class AutoCollection {
+    public static void main(String[] args) {
+        //strong, soft, weak, fantom references
+        processAnonimous();
+        processNamed();
+    }
+
+    public static void processAnonimous(){
+        for (int i = 0; i < 100; i++) {
+            new Thing().show();
+            {
+                new Thing().show();
+                new Thing().show();
+            }
+            new Thing().show();
+            new Thing().show();
+        }
+    }
+
+    public static void processNamed(){
+        Thing thing1, thing2, thing3, thing4, thing5, thing6;
+        thing1 = new Thing(); thing1.show();
+        thing2 = new Thing(); thing2.show();
+        {
+
+            thing3=new  Thing(); thing3.show();
+            thing4=new  Thing(); thing4.show();
+        }
+        thing5=new  Thing(); thing5.show();
+        thing6=new  Thing(); thing6.show();
+        new Thing().show();
+    }
+}
+
+class Thing{
+    private static final String names[] = {
+            "Stapler","Eraser","Pen", "Pencil", "Marker", "Calculator", "Glue", "Scissors","Notebook","Envelope"};
+    private static int counter = 0;
+    private static long heapFreeSize = Runtime.getRuntime().freeMemory();
+    private int id;
+    private String name;
+
+    public Thing(){
+        id = counter++;
+        name = names[(int) Math.floor(Math.random()*names.length)];
+    }
+
+
+    @Override
+    public String toString() {
+        heapFreeSize = Runtime.getRuntime().freeMemory();
+
+        return "Thing{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", heapFreeSize=" + heapFreeSize +
+                '}';
+    }
+
+    public void show(){
+        System.out.println(toString());
+    }
+
+    @Override
+    protected void finalize()
+    {
+        --counter;
+    }
+}
